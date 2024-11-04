17b27b49c3a547bce00cf2d47f12ee98b599a8e6
Sarah <sarahireneclark@gmail.com>
2024-11-03 18:10:05 -0600

    Color and frames

--- GuessFlag/GuessFlag/ContentView.swift
+++ GuessFlag/GuessFlag/ContentView.swift
@@ -10,9 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Text("Hello, World!")
-            Text("This is inside a stack")
+            VStack(spacing: 0) {
+                Color.red
+                Color.blue
+            }
+            Text("Your content")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
         }
+        .ignoresSafeArea()
     }
 }
 
