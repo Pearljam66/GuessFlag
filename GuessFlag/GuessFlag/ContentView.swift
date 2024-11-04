eb05874a30e5834fe4687427fbd2014dda59133d
Sarah <sarahireneclark@gmail.com>
2024-11-04 14:26:03 -0600

    Buttons and images

--- GuessFlag/GuessFlag/ContentView.swift
+++ GuessFlag/GuessFlag/ContentView.swift
@@ -9,10 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Your content")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .foregroundStyle(.white)
-            .background(.red.gradient)
+        VStack {
+            Button("Button 1") {}
+                .buttonStyle(.bordered)
+            Button("Button 2", role: .destructive) {}
+                .buttonStyle(.bordered)
+            Button("Button 3") {}
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            Button("Button 4", role: .destructive) {}
+                .buttonStyle(.borderedProminent)
+            Button {
+                print("Button was tapped")
+            } label: {
+                Text("Tap me!")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.red)
+            }
+            Button {
+                print("Edit button was tapped")
+            } label: {
+                Label("Edit", systemImage: "pencil")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.red)
+            }
+        }
 
     }
 }
